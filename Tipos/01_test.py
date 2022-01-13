48a5fd3ec910abdca5aff390dcc338a26a941dfd
Giancarlo <giancarlobenavides@hotmail.com>
2022-01-13 17:12:33 -0500

    init

---
+++ Tipos/01_test.py
@@ -0,0 +1,24 @@
+# test_01.py
+''' OPERACIONES CON CADENAS
+Ejemplo para el curso de métodos numéricos
+por Ing. Giancarlo Ortiz. '''
+# Operaciones con cadenas en python 3
+""" En programación, las operaciones mas básicas incluyen operaciones con cadenas.
+Python usa la codificación Unicode en lugar de ASCII.
+Lo que hace posibles mas de 100.000 Caracteres.
+Type("x") → <class 'str'> → cadena Unicode. """
+
+# Importando funcionalidad al script 
+import sys
+
+# Asignaciones y operaciones con cadenas
+info = sys.version
+version = info.split(" ")[0]
+plataforma = info[info.find("[")+1:len(info)-1]
+
+# Salida estándar
+print("---------------------------------------")
+print(f"la instalación de Python {version} fue exitosa")
+print(f"Tu plataforma elegida es {plataforma}...")
+print(f"buena suerte como pythonista", "...")
+print("---------------------------------------")
